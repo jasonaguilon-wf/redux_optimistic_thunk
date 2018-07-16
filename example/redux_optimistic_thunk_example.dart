151a217f3014458a6c977100e2390276b0a441fd
Jason Aguilon <jason.aguilon@webfilings.com>
2018-07-16 16:23:57 -0600

    Single optimistic manager per store
    Required in order to properly handle rollback on error in the face of
    multiple optimistic states being posted.

--- example/redux_optimistic_thunk_example.dart
+++ example/redux_optimistic_thunk_example.dart
@@ -1,79 +1,83 @@
-import 'dart:async';
-import 'package:redux/redux.dart' show NextDispatcher, Store;
+import 'dart:async' show Future;
+import 'package:redux/redux.dart' show Middleware, NextDispatcher, Store;
 
-import 'package:redux_optimistic_thunk/redux_optimistic_thunk.dart' show Optimistic, OptimisticAction, OptimisticMark, OptimisticRollback, OptimisticThunkAction, createOptimisticReducer, optimisticThunk;
+import 'package:redux_optimistic_thunk/redux_optimistic_thunk.dart'
+    show Optimistic, OptimisticAction, OptimisticMark, OptimisticRollback, OptimisticThunkAction, createOptimisticReducer, optimisticThunk;
 
 class Push {
   final String value;
 
   Push(this.value);
+
+  @override
+  String toString() => 'Push(value: "$value")';
 }
 
 List<String> reducer(List<String> state, dynamic action) {
   if (action is Push) {
-    return state..toList()..add(action.value);
+    return state
+      ..toList()
+      ..add(action.value);
   }
   return state;
 }
 
-void logger(Store<Optimistic<List<String>>> store, dynamic action, NextDispatcher next) {
-  if (!(action is Push || action is OptimisticAction)) {
-    next(action);
-    return;
-  }
+Middleware<Optimistic<List<String>>> logger() {
+  DateTime start;
 
-  next(action);
-  
-  var prints = store.state.state.map((value) => value.contains('optimi') ? '($value)' : '_${value}_');
-  var prefix = () {
-     if (action is OptimisticRollback) {
-       return '  (rollback)';
-     }
-     else if (action is OptimisticMark) {
-       return '      (mark)';
-     }
-     else {
-       return store.state.isPending ? '(optimistic)' : '    (actual)';
-     }
-  }();
-  print('$prefix ${prints.join(' -> ')}');
+  return (Store<Optimistic<List<String>>> store, dynamic action, NextDispatcher next) {
+    var now = new DateTime.now();
+    start ??= now;
+    var diff = now.difference(start);
+
+    if (!(action is Push || action is OptimisticAction)) {
+      next(action);
+      return;
+    }
+
+    next(action);
+
+    var prints = store.state.state.map((value) => value.contains('optimi') ? '($value)' : '_${value}_');
+    var prefix = () {
+      if (action is OptimisticRollback) {
+        return '  (rollback)';
+      }
+      else if (action is OptimisticMark) {
+        return '      (mark)';
+      }
+      else {
+        return store.state.isPending ? '(optimistic)' : '    (actual)';
+      }
+    }();
+    print('$diff $prefix ${prints.join(' -> ')}');
+  };
 }
 
-final store = new Store<Optimistic<List<String>>>(createOptimisticReducer(reducer), initialState: new Optimistic(<String>[]), middleware: [optimisticThunk, logger]);
+final store = new Store<Optimistic<List<String>>>(
+    createOptimisticReducer(reducer), initialState: new Optimistic(<String>[]),
+    middleware: [optimisticThunk(), logger()]);
 
 main() async {
   var slow = new OptimisticThunkAction(
-          (void dispatch(dynamic action)) async {
-        dispatch(new Push('slow actual 1'));
-        dispatch(new Push('slow actual 2'));
-
-        await new Future.delayed(new Duration(milliseconds: 500));
-
-        dispatch(new Push('slow actual 3'));
-        dispatch(new Push('slow actual 4'));
+          (void dispatch([dynamic action])) async {
+        await new Future.delayed(new Duration(milliseconds: 5000));
+        dispatch(new Push('actual 1'));
       },
           (void dispatch(dynamic action)) {
-        dispatch(new Push('slow optimi 1'));
-        dispatch(new Push('slow optimi 2'));
+        dispatch(new Push('optimi 1'));
       }
   );
 
-  var fast = new OptimisticThunkAction(
-          (void dispatch(dynamic action)) async {
-        dispatch(new Push('fast actual 1'));
-        dispatch(new Push('fast actual 2'));
-
-        await new Future.delayed(new Duration(milliseconds: 200));
-
-        dispatch(new Push('fast actual 3'));
-        dispatch(new Push('fast actual 4'));
+  var error = new OptimisticThunkAction(
+          (dispatch([dynamic action])) async {
+        await new Future.delayed(new Duration(milliseconds: 10000));
+        dispatch(); // on error, dispatch no action to allow optimistic state to rollback.
       },
           (void dispatch(dynamic action)) {
-        dispatch(new Push('fast optimi 1'));
-        dispatch(new Push('fast optimi 2'));
+        dispatch(new Push('optimi 2'));
       }
   );
 
   store.dispatch(slow);
-  store.dispatch(fast);
+  store.dispatch(error);
 }
--- lib/src/redux_optimistic_thunk_base.dart
+++ lib/src/redux_optimistic_thunk_base.dart
@@ -1,51 +1,63 @@
+import 'dart:async';
 import 'package:redux/redux.dart';
 import 'package:redux_optimistic_thunk/src/redux_optimistic_manager_base.dart';
 
-void optimisticThunk<State>(Store<Optimistic<State>> store, dynamic action, NextDispatcher next) {
-    var manager = new OptimisticManager(store);
+Middleware<Optimistic<State>> optimisticThunk<State>() {
+    OptimisticManager manager;
 
-    final int transactionId = identityHashCode(new Object());
+    return (Store<Optimistic<State>> store, dynamic action, NextDispatcher next) {
+        manager ??= new OptimisticManager(store);
 
-    if (action is! OptimisticThunkAction) {
-        return next(manager.postAction(action));
-    }
+        assert(identical(manager.store, store));
 
-    bool isActualThunkReturned = false;
-    bool isOptimisticThunkReturned = false;
-    bool isOptimisticStateRollbacked = false;
-
-    void actualDispatch(action) {
-        // Rollback optimistic state on first async dispatch
-        if (isActualThunkReturned && !isOptimisticStateRollbacked) {
-            isOptimisticStateRollbacked = true;
-            manager.rollback(transactionId, next);
+        if (action is! OptimisticThunkAction) {
+            next(manager.postAction(action));
+            return;
         }
 
-        // Allow call `dispatch` without argument to rollback optimistic actions
-        next(manager.postAction(action));
+        bool isActualThunkReturned = false;
+        bool isOptimisticThunkReturned = false;
+        bool isOptimisticStateRollbacked = false;
+
+        final int transactionId = action.transactionId;
+
+        void actualDispatch([dynamic action]) {
+            // Rollback optimistic state on first async dispatch
+            if (isActualThunkReturned && !isOptimisticStateRollbacked) {
+                isOptimisticStateRollbacked = true;
+                manager.rollback(transactionId, next);
+            }
+
+            // Allow call `dispatch` without argument to rollback optimistic actions
+            if (action != null) {
+                next(manager.postAction(action));
+            }
+        };
+
+        void optimisticDispatch(action) {
+            if (isOptimisticThunkReturned) {
+                throw new ArgumentError.value(action, 'action', 'Optimistic thunk must be a sync function');
+            }
+
+            return next(manager.postAction(action, transactionId));
+        };
+
+        // First call actual thunk to ensure all sync actions are flushed
+        action.actualThunk(actualDispatch);
+        isActualThunkReturned = true;
+        // Then call optimistic thunk to create optimistic state
+        action.optimisticThunk(optimisticDispatch);
+        isOptimisticThunkReturned = true;
     };
-
-    void optimisticDispatch(action) {
-        if (isOptimisticThunkReturned) {
-            throw new ArgumentError.value(action, 'action', 'Optimistic thunk must be a sync function');
-        }
-
-        return next(manager.postAction(action, transactionId));
-    };
-
-    // First call actual thunk to ensure all sync actions are flushed
-    action.actualThunk(actualDispatch);
-    isActualThunkReturned = true;
-    // Then call optimistic thunk to create optimistic state
-    action.optimisticThunk(optimisticDispatch);
-    isOptimisticThunkReturned = true;
 }
 
 class OptimisticThunkAction {
   final _actualThunk;
   final _optimisticThunk;
 
-  OptimisticThunkAction(void actualThunk(dynamic action), void optimisticThunk(dynamic action))
+  final int transactionId = identityHashCode(new Object());
+  
+  OptimisticThunkAction(Future<Null> actualThunk(void dispatch([dynamic action])), void optimisticThunk(dispatch(dynamic action)))
       : _actualThunk = actualThunk, _optimisticThunk = optimisticThunk;
 
   dynamic actualThunk(dynamic action) => _actualThunk(action);
